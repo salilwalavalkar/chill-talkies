e0e72374c38e93dca59ee1b3f2b6f57a495c07a8
salil <salil@xyz>
2019-06-08 18:33:30 +0100

    Added swagger 2 for api documentation

--- src/main/java/com/chill/talkies/web/MovieController.java
+++ src/main/java/com/chill/talkies/web/MovieController.java
@@ -1,6 +1,7 @@
 package com.chill.talkies.web;
 
 import com.chill.talkies.domain.Movie;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
@@ -21,30 +22,35 @@ public class MovieController {
         this.repository = repository;
     }
 
+    @ApiOperation(value = "Get all movies", response = Iterable.class)
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Movie> movies() {
         log.info("Getting all movies");
         return repository.findAll();
     }
 
+    @ApiOperation(value = "Add a movie", response = Movie.class)
     @RequestMapping(method = RequestMethod.PUT)
     public Movie add(@RequestBody @Valid Movie movie) {
         log.info("Adding movie: {}", movie);
         return repository.save(movie);
     }
 
+    @ApiOperation(value = "Update a movie", response = Movie.class)
     @RequestMapping(method = RequestMethod.POST)
     public Movie update(@RequestBody @Valid Movie movie) {
         log.info("Updating movie: {}", movie);
         return repository.save(movie);
     }
 
+    @ApiOperation(value = "Get a movie by id", response = Movie.class)
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Optional<Movie> getById(@PathVariable String id) {
         log.info("Getting movie by id: {}", id);
         return repository.findById(id);
     }
 
+    @ApiOperation(value = "Delete a movie by id")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteById(@PathVariable String id) {
         log.info("Deleting movie by id: {}" + id);
